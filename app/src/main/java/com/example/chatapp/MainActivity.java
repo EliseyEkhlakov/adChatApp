92c7d364c89dd0bde5956abffc53a42e4802970c
Elisey Ekhlakov <ekhalakovea@bk.ru>
2021-09-12 22:32:09 +0400

    +Firebase

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.chatapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,31 +10,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseFirestore db;
     private RecyclerView recyclerViewMessages;
     private MessagesAdapter adapter;
     private EditText editTextMessage;
     private ImageView imageViewSendMessage;
 
-    private List<Message> messages;
     private String author;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
         recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
         editTextMessage = findViewById(R.id.editTextMessage);
         imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
         adapter = new MessagesAdapter();
         recyclerViewMessages.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewMessages.setAdapter(adapter);
-        messages = new ArrayList<>();
+
         author = "C'est moi";
         imageViewSendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,15 +52,38 @@ public class MainActivity extends AppCompatActivity {
                 sendMessage();
             }
         });
+        db.collection("messages")
+                .orderBy("date")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        if (queryDocumentSnapshots != null) {
+                            List<Message> messages = queryDocumentSnapshots.toObjects(Message.class);
+                            adapter.setMessages(messages);
+                        }
+                    }
+                });
     }
 
     private void sendMessage(){
         String textOfMessage = editTextMessage.getText().toString().trim();
         if(!textOfMessage.isEmpty()){
-            messages.add(new Message(author, textOfMessage));
-            adapter.setMessages(messages);
-            editTextMessage.setText("");
+
             recyclerViewMessages.scrollToPosition(adapter.getItemCount()-1);
+            db.collection("messages")
+                    .add(new Message(author, textOfMessage, System.currentTimeMillis()))
+            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    editTextMessage.setText("");
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
