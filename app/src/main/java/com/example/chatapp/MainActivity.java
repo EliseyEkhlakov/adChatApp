d8c09292328c9ce9ce5f2099d06fd45742442e99
Elisey Ekhlakov <ekhalakovea@bk.ru>
2021-09-15 12:32:29 +0400

    +Auth

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,6 +17,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -30,14 +34,30 @@ public class MainActivity extends AppCompatActivity {
     private MessagesAdapter adapter;
     private EditText editTextMessage;
     private ImageView imageViewSendMessage;
-
     private String author;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.itemSignOut) {
+            mAuth.signOut();
+            signOut();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
         editTextMessage = findViewById(R.id.editTextMessage);
         imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
@@ -63,27 +83,38 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+        if (mAuth.getCurrentUser() != null) {
+            Toast.makeText(this, "Logged", Toast.LENGTH_SHORT).show();
+        } else {
+            signOut();
+        }
+
     }
 
-    private void sendMessage(){
+    private void sendMessage() {
         String textOfMessage = editTextMessage.getText().toString().trim();
-        if(!textOfMessage.isEmpty()){
+        if (!textOfMessage.isEmpty()) {
 
-            recyclerViewMessages.scrollToPosition(adapter.getItemCount()-1);
+            recyclerViewMessages.scrollToPosition(adapter.getItemCount() - 1);
             db.collection("messages")
                     .add(new Message(author, textOfMessage, System.currentTimeMillis()))
-            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                @Override
-                public void onSuccess(DocumentReference documentReference) {
-                    editTextMessage.setText("");
-                }
-            })
-            .addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                }
-            });
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            editTextMessage.setText("");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    });
         }
+    }
+
+    private void signOut() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
